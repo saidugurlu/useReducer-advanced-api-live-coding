b9dbf553fce0e4bddaf67974417dbc0f8d9a2b86
saidugurlu <ugurlumsaid@gmx.de>
2022-05-17 11:09:58 +0200

    book data showing without styles

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -4,10 +4,39 @@ const techBooksUrl = 'https://edwardtanguay.netlify.app/share/techBooks.json';
 
 export const PageBooks = () => {
 	const [techBooks, setTechBooks] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await fetch(techBooksUrl);
+			const _techBooks = await response.json();
+			setTechBooks(_techBooks);
+		})();
+	}, []);
+
 	return (
 		<div className="page_books">
 			<h2>Books</h2>
 			<p>I am reading the following {techBooks.length} tech books:</p>
+			<div className="techBooks">
+				{techBooks.map((techBook, index) => {
+					return (
+						<div className="techBook">
+							<img
+								src={`https://edwardtanguay.netlify.app/share/images/techBooks/${techBook.idCode}.jpg`}
+								alt="book cover"
+							/>
+							<div className="info">
+								<div key={index} className="techBook">
+									{techBook.title}
+								</div>
+								<div className="description">
+									{techBook.description}
+								</div>
+							</div>
+						</div>
+					);
+				})}
+			</div>
 		</div>
-	)
-}
+	);
+};
